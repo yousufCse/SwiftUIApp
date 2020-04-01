a20f78c36c9fdb6855dc9392817306ea5934b780
Md. Yousuf Ali <yousuf@users-MacBook-Pro.local>
2020-04-01 09:14:49 +0600

    Generate default files

---
+++ SwiftUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Md. Yousuf Ali on 24/3/20.
+//  Copyright © 2020 Md. Yousuf Ali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Hello SwiftUI")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
